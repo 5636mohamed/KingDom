fd92750275819bf0f1771c190e37fea23d780acb
5636mohamed <85107448+5636mohamed@users.noreply.github.com>
2021-08-03 17:01:00 +0200

    this is the new edition

--- js/plugins.js
+++ js/plugins.js
@@ -1,6 +1,14 @@
 let menu = document.querySelector(".overview nav .links ul"),
-  theClicker = document.querySelector(".overview nav .links .sign-content");
+  theClicker = document.querySelector(".overview nav .links .sign-content"),
+  theButton = document.querySelector(".link"),
+  theScreen = document.querySelector(".about .screen"),
+  theImage = document.querySelector(".about .container .image img"),
+  overlay = theImage.nextElementSibling;
 
 theClicker.onclick = () => {
   menu.classList.toggle("animated");
 };
+
+theImage.onclick = () => overlay.style.cssText = `height: 100%; color: #fff;`;
+
+overlay.onclick = () => overlay.style.cssText = `heigh: 0; color: transparent;`;
