0aec29ba1bc04d3b909a92e170d96f5f125809ee
5636mohamed <85107448+5636mohamed@users.noreply.github.com>
2021-08-24 16:10:35 +0200

    this is the edition

--- js/plugins.js
+++ js/plugins.js
@@ -1,6 +1,6 @@
 let menu = document.querySelector(".overview nav .links ul"),
   theClicker = document.querySelector(".overview nav .links .sign-content"),
-  theButton = document.querySelector(".link"),
+  theButton = document.querySelectorAll(".link"),
   theScreen = document.querySelector(".about .screen"),
   theImage = document.querySelector(".about .container .image img"),
   overlay = theImage.nextElementSibling;
@@ -12,3 +12,9 @@ theClicker.onclick = () => {
 theImage.onclick = () => overlay.style.cssText = `height: 100%; color: #fff;`;
 
 overlay.onclick = () => overlay.style.cssText = `heigh: 0; color: transparent;`;
+
+function helloWorld(){
+  console.log("hello, world");
+}
+
+theButton.forEach(theButton, button, (button, i) => button[i].onclick = helloWorld());
